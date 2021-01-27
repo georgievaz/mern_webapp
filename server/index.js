55fc2fa8b874329b059db7ad9a8ec0f5f025f1e6
z.georgieva <zornitsa.zh.georgieva@gmail.com>
2021-01-27 23:29:15 +0100

    backend set up

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+
+//initalize the app 
+const app = express(); 
+
+
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors()); 
+app.use('/posts', postRoutes);
+// Cloud database https://mongodb.com/cloud/atlas 
+
+const CONNECTION_URL = 'mongodb+srv://zgeorgieva:1234papa@cluster0.9jxxk.mongodb.net/<dbname>?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000; 
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(()=> app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
